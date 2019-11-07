4cc3ace4c41f58574de0bd310a50f7f2c87060bd
wennie0919 <55677768+wennie0919@users.noreply.github.com>
2019-11-07 23:08:21 +0800

    Create mergesort.py

---
+++ HW2/mergesort.py
@@ -0,0 +1,48 @@
+array = [2,-3,4,11,-6,5.3,7]
+class Solution(object):
+    def mergesort(self,array):
+        if len(array) < 1:        
+            return array
+        else:        
+            middle=len(array)//2
+            left_array=array[:middle]
+            right_array=array[middle:]
+            
+            self.mergesort(left_array)
+            self.mergesort(right_array)   
+            
+       
+        a=0
+        b=0
+        c=0
+        d=[]
+        
+        if left_array:     
+                d+=left_array
+        else:
+                d+=right_array
+                
+                
+        while a <len(left) and b<len(right):
+            if left[a] < right[b]:
+                array[c] = left[a]
+                a+=1
+               
+            else:
+                array[c] = right[b]
+                b+=1
+                c+=1
+            
+        while a < len(left) or q < len(right):
+            if a < len(left):
+                array[c] = left[a]
+                a+=1
+                c+=1
+            else:
+                array[c] = right[b]
+                b+=1
+                c+=1 
+                
+            
+
+        return array
