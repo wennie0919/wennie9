9f15a6b125fb0bba061255db16126d0bdfc14740
wennie0919 <55677768+wennie0919@users.noreply.github.com>
2019-12-20 17:47:30 +0800

    Update BFS_06170146.py

--- HW5/BFS_06170146.py
+++ HW5/BFS_06170146.py
@@ -1,30 +1,40 @@
-# Python3 Program to print BFS traversal 
-# from a given source vertex. BFS(int s) 
-# traverses vertices reachable from s. 
 from collections import defaultdict 
   
-# This class represents a directed graph 
-# using adjacency list representation 
 class Graph:
-    # Constructor 
-    def __init__(self): 
-        # default dictionary to store graph 
+    def __init__(self):       
         self.graph = defaultdict(list) 
 
-    # function to add an edge to graph 
     def addEdge(self,u,v): 
         self.graph[u].append(v) 
   
-    # Function to print a BFS of graph 
+
     def BFS(self, s): 
-        """
-        :type s: int
-        :rtype: list
-        """
-    def DFS(self, s):
-        """
-        :type s: int
-        :rtype: list
-        """
-  
+        queue = [s]
+        answer= []
         
+        while queue:
+            s=queue.pop(0)
+            answer.append(s)
+            for a in self.graph[s]:
+                    if a not in answer:
+                        queue.append(a)
+        return answer
+        
+    def DFS(self, s):
+        answer = []
+        stack = [s]
+
+        while stack:
+            s = stack.pop(-1)
+            if s not in answer:
+                answer.append(s)
+                neighbours = self.graph[s]
+                for neighbour in neighbours:
+                    stack.append(neighbour)
+        return answer
+
+        
+###參考資料：https://kopu.chat/2017/09/22/%E5%AF%A6%E4%BD%9Cgraph%E8%88%87dfs%E3%80%81bfs%E8%B5%B0%E8%A8%AA/
+### https://magiclen.org/dfs-bfs/
+### https://www.itread01.com/content/1541297601.html
+### https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/102866/
