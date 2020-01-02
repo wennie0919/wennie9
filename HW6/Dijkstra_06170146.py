d673ffef8bda0425954d84daf77b1cf5320ae943
wennie0919 <55677768+wennie0919@users.noreply.github.com>
2020-01-03 00:25:21 +0800

    Update Dijkstra_06170146.py

--- HW6/Dijkstra_06170146.py
+++ HW6/Dijkstra_06170146.py
@@ -1,1 +1,17 @@
-ed
+from collections import defaultdict 
+
+#Class to represent a graph 
+class Graph(): 
+
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [] 
+        self.graph_matrix = [[0 for column in range(vertices)]  
+                    for row in range(vertices)] 
+    def addEdge(self,u,v,w): 
+      
+    def Dijkstra(self, s): 
+      
+
+    def Kruskal(self):
+      
