07c477c439b8e6f70967dd4475eade7a6476991a
wennie0919 <55677768+wennie0919@users.noreply.github.com>
2020-01-03 23:30:41 +0800

    Update Dijkstra_06170146.py

--- HW6/Dijkstra_06170146.py
+++ HW6/Dijkstra_06170146.py
@@ -1,17 +1,42 @@
 from collections import defaultdict 
+import sys
+ 
+class Graph():
+ 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)] 
+                      for row in range(vertices)]
+    
+    def printSolution(self, dist, s):
+        print ("Vertex to Distance from Source")
+        for node in range(self.V):
+            print (s," to destination ",node, "'s distance is", dist[node])
+ 
+    def minDistance(self, dist, sptSet):
+        min = sys.maxsize
 
-#Class to represent a graph 
-class Graph(): 
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+ 
+        return min_index
 
-    def __init__(self, vertices): 
-        self.V = vertices 
-        self.graph = [] 
-        self.graph_matrix = [[0 for column in range(vertices)]  
-                    for row in range(vertices)] 
-    def addEdge(self,u,v,w): 
-      
-    def Dijkstra(self, s): 
-      
+    def Dijkstra(self, s):
+ 
+        dist = [sys.maxsize] * self.V
+        dist[s] = 0
+        sptSet = [False] * self.V
+ 
+        for cout in range(self.V):
+            u = self.minDistance(dist, sptSet)
+            sptSet[u] = True
 
-    def Kruskal(self):
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]:
+                        dist[v] = dist[u] + self.graph[u][v]
+ 
+        self.printSolution(dist, s)
+    
       
