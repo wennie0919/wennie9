12a6fa3135dfb89ce9365dc914ac063c766ecf05
wennie0919 <55677768+wennie0919@users.noreply.github.com>
2020-01-10 23:23:32 +0800

    Create 14_Longest Common Prefix_06170147.py

---
+++ leetCode/14_Longest Common Prefix_06170147.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        strs.sort()
+        res = ''
+        for i in xrange(len(strs[0])):
+            if i >= len(strs[-1]) or strs[-1][i] != strs[0][i]:
+                return res
+            res += strs[0][i]
+        return res
